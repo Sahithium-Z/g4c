f536d73686555285bfc57cc2619c4b9a9b39511b
FriedKarp <115669324+FriedKarp@users.noreply.github.com>
2023-02-08 14:36:35 -0800

    added sand texture
    sand

--- Main.gd
+++ Main.gd
@@ -4,7 +4,7 @@ onready var tileMap = $TileMap
 
 func shrink_island():
 	# Get all sand cells
-	var cells = tileMap.get_used_cells_by_id(2)
+	var cells = tileMap.get_used_cells_by_id(4)
 	# Store cells that are going to get flooded in this table
 	var cells_to_flood = []
 	
@@ -16,6 +16,9 @@ func shrink_island():
 	# Flood cells once water checks are done
 	for cell in cells_to_flood:
 		tileMap.set_cellv(cell, 3)
+	
+	# Update autotiles
+	tileMap.update_bitmask_region()
 
 func check_water(cell): # Checks adjacent cells for water, trust me the really long if statement is worth it
 	if tileMap.get_cellv(cell + Vector2.UP) == 3 or tileMap.get_cellv(cell + Vector2.LEFT) == 3 or tileMap.get_cellv(cell + Vector2.DOWN) == 3 or tileMap.get_cellv(cell + Vector2.RIGHT) == 3:
