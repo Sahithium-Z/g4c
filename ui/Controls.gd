22a24a02b3a2b5089784acb5d11c609e9f27269c
Sahithium-Z <Sahithium-Z+git@pm.me>
2023-04-18 21:03:40 -0700

    added controls in top right

--- ui/Controls.gd
+++ ui/Controls.gd
@@ -3,7 +3,7 @@ extends Control
 onready var world = get_tree().get_root().find_node("World", true, false)
 onready var player = get_tree().get_root().find_node("Player", true, false)
 onready var label = $Panel/Label
-var controls = ["How to Play:\nYou are on an island.\nYour goals are to make as much money as possible before the island sinks.", "Controls:\nR = Till Dirt\nT = Plant Seeds / Harvest Plant\nV = Sell Plants\nB = Buy Seeds","Sleep:\nPress Enter next to the tent to go to sleep.","Fertilizer:\nFor $40 you can buy fertilizer by pressing \"m\".\nThis grows all crops instantly.","Floodbarrier:\nThis upgrade can be bought for $40 by pressing \"n\".\nIt stops water from rising for 1 turn", ""]
+var controls = ["How to Play:\nYou are on an island.\nYour goals are to make as much money as possible before the island sinks.", "Controls:\nR = Till Dirt\nT = Plant Seeds / Harvest Plant\nB = Open Shop","Sleep:\nPress Enter next to the tent to go to sleep.","Fertilizer:\nFor $40 you can buy fertilizer in the shop.\nThis grows all crops instantly.","Floodbarrier:\nThis upgrade can be bought for $40 in the shop.\nIt stops water from rising for 1 turn", ""]
 var control_id = 0
 var control_active = false
 func _ready():
